93f22cc488256f286442e05922ec85d875bab83e
2020mt93220 <2020mt93220@wilp.bits-pilani.ac.in>
2021-10-30 10:37:00 +0530

    adding data

--- vpc/nat_gateway.tf
+++ vpc/nat_gateway.tf
@@ -33,7 +33,7 @@ resource "aws_eip" "nat_gateway" {
 
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway.id
-  subnet_id = aws_subnet.public.id
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   tags = {
     "Name" = "Nat Gateway"
   }
