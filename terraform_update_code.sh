4b5dadbb0be459b6729ff77aae353f5803c40bb6
2020mt93220 <2020mt93220@wilp.bits-pilani.ac.in>
2021-10-30 12:26:09 +0530

    adding new files

--- terraform_update_code.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-terraform init -backend-config=environments/demo/backend.config
-terraform plan -var-file=environments/demo/variables.tfvars
-terraform apply -var-file=environments/demo/variables.tfvars --auto-approve=true
-terraform destroy -var-file=environments/demo/variables.tfvars  --auto-approve=true
