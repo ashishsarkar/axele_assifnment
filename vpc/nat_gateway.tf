99ef58c7fe8d45223573ba8180be64370e4ff9c7
2020mt93220 <2020mt93220@wilp.bits-pilani.ac.in>
2021-10-30 10:57:24 +0530

    adding new commit data

--- vpc/nat_gateway.tf
+++ vpc/nat_gateway.tf
@@ -29,11 +29,13 @@
 
 resource "aws_eip" "nat_gateway" {
   vpc = true
+  depends_on = [aws_internet_gateway.main]
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway.id
-  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  subnet_id      = aws_subnet.public.id
+  depends_on = [aws_internet_gateway.main]
   tags = {
     "Name" = "Nat Gateway"
   }
